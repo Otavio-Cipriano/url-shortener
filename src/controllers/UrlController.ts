666fb1c66aec24df9e7b0c8161cfb82cfcfa9bb4
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2022-10-23 05:13:12 -0300

    views styles

--- src/controllers/UrlController.ts
+++ src/controllers/UrlController.ts
@@ -1,16 +1,18 @@
 import UrlService from '@services/UrlService'
 import { Request, Response } from 'express'
+import { Types } from 'mongoose'
+import renderView from 'src/utils/renderView'
 import validator from 'validator'
 
 export default class UrlController {
   public static async registerLink (req: Request, res: Response) {
-    const body = req.body
-    if (!body?.url) return res.status(400).send({ error: 'You must provide a URl' })
+    const { url } = req.body
+    if (url) return res.status(400).send({ error: 'You must provide a URl' })
 
-    const isValid = validator.isURL(body.url)
+    const isValid = validator.isURL(url)
     if (!isValid) return res.status(400).send({ error: 'URL Provided is no valid' })
 
-    const result = await UrlService.registerUrl(body.url)
+    const result = await UrlService.registerUrl(url)
     if (!result?.success) return res.status(406).send({ error: 'something went wrong at registering url' })
 
     const data = result.data
@@ -25,7 +27,8 @@ export default class UrlController {
   public static async getLink (req: Request, res: Response) {
     const { id } = req.params
 
-    if (id) return res.status(400).send({ error: 'Ops, This page doesn\'t exist' })
+    if (!id) return renderView('page404', res, 404)
+    if (!Types.ObjectId.isValid(id)) return renderView('page404', res, 404)
 
     const query = await UrlService.getUrl(id)
 
--- src/utils/renderView.ts
+++ src/utils/renderView.ts
@@ -1,14 +1,14 @@
 import { Response } from 'express'
 import fs from 'fs'
 
-export default function renderView (view: string, res: Response) {
+export default function renderView (view: string, res: Response, status?: number, dataInserction?: Object) {
   const path = `./src/views/${view}.ejs`
   fs.access(path, (err) => {
     if (err) return res.send('This View doesn\'t exist')
 
     fs.readFile(path, 'ascii', (err, data) => {
       if (err) return res.send('This View doesn\'t exist')
-      return res.render('layout.ejs', { content: data })
+      return res.status(status ?? 200).render('layout.ejs', { content: data, ...dataInserction })
     })
   })
 }
